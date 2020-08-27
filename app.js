aff6aa55c5a833b6e3827d1195ddf982251bc8f2
Linzi Carlin <linzi.carlin@infinityworks.com>
2020-08-27 10:26:15 +0100

    add updates to css, dynamic copyright date

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-//
+//function to place a piece
+//when arrow clicked piece appears at last empty position in that coloumn
+//loop through positions. On each iteration look ahead to next position. If not empty change colour of
+//current position to red/yellow depending on turn
