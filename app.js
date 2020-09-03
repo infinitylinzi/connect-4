71d7fa4f191d3e5a22884703e7734329fc6abc6c
Linzi Carlin <linzi.carlin@infinityworks.com>
2020-09-03 11:17:46 +0100

    add form and break everything

--- app.js
+++ app.js
@@ -1,111 +1,103 @@
-//function to place a piece
-//when arrow clicked piece appears at last empty position in that coloumn
+/* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-plusplus */
+// function to place a piece
+// when arrow clicked piece appears at last empty position in that coloumn
 
-let numRows = 6
-let numColumns = 7
+function getFormSize() {
+  const userInputRows = $('#rows').value;
+  const userInputCols = $('#columns').value;
 
-drawGrid();
-
-const arrowButtons = $(".arrow-grid")
-
-const arrowArray = Array.from(arrowButtons)
-
-for(let i=0; i<arrowArray.length; i++){
-  
-  arrowArray[i].addEventListener('click', function(){
-    let arrowButton = i
-    
-    arrowClick(arrowButton, event)
-    
-  })
+  $('#grid-submit').click((event) => {
+    event.preventDefault();
+    drawGrid(userInputRows, userInputCols);
+  });
 }
 
-function arrowClick(arrowButton, event){
-  
-  event.preventDefault()
-  
-  const rows = numRows
-  
-  const columnArray = []
-  
-  for(let i=0; i<rows; i++){
-    let columnPosition = $("#row-" +i + "-column-" + arrowButton)
-    columnArray.push(columnPosition)
-    console.log(columnArray)
-  
-      }
+function arrowClick(arrowButton, event) {
+  event.preventDefault();
 
-      for (let i=0; i<columnArray.length-1; i++){
-        //loop through positions. Get class of inner span element. If class === reddot, change class of current position
-        //to reddot and break
-        let thisSpace = columnArray[i]
-        let nextSpace = columnArray[i+1]
+  // define rows with user input rows
 
-        if($(nextSpace).children("span").hasClass("reddot")) {
-          $(thisSpace).children("span").removeClass("whitedot")
-          $(thisSpace).children("span").addClass("reddot")
-          break
-      }
+  const columnArray = [];
 
-        }
-        
-        const rowNumber = String(rows-1)
-        let currentRow = $("#row-"+rowNumber+"-column-"+arrowButton).children("span")
+  for (let i = 0; i < rows; i++) {
+    const columnPosition = $(`#row-${i}-column-${arrowButton}`);
+    columnArray.push(columnPosition);
+    console.log(columnArray);
+  }
 
-        $(currentRow).removeClass("whitedot")
-        $(currentRow).addClass("reddot")
-        
-        
-      }
+  for (let i = 0; i < columnArray.length - 1; i++) {
+    // eslint-disable-next-line max-len
+    // loop through positions. Get class of inner span element. If class === reddot,
+    // change class of current position
+    // to reddot and break
+    const thisSpace = columnArray[i];
+    const nextSpace = columnArray[i + 1];
 
+    if ($(nextSpace).children('span').hasClass('reddot')) {
+      $(thisSpace).children('span').removeClass('whitedot');
+      $(thisSpace).children('span').addClass('reddot');
+      break;
+    }
+  }
 
+  const rowNumber = String(rows - 1);
+  const currentRow = $(`#row-${rowNumber}-column-${arrowButton}`).children('span');
 
-//make grid of size x by y
+  $(currentRow).removeClass('whitedot');
+  $(currentRow).addClass('reddot');
+}
 
+// make grid of size x by y
 
-function drawGrid() {
-  let rows = numRows;
-  let columns = numColumns;
-
+function drawGrid(rows, columns) {
   for (let i = 0; i < columns; i++) {
-//     let arrowButton = $(
-//       "<span><div class='arrow-grid'><i class='fas fa-arrow-alt-circle-down fa-2x arrow-colour'></i></div></span>"
-//     );
-
-let arrowButton = $("<span class='arrow-grid'></span").append("<div></div>").append("<i class='fas fa-arrow-alt-circle-down fa-2x arrow-colour'></i>")
-    $("#arrow-buttons").append(arrowButton);
+    const arrowButton = $("<span class='arrow-grid'></span").append('<div></div>').append("<i class='fas fa-arrow-alt-circle-down fa-2x arrow-colour'></i>");
+    $('#arrow-buttons').append(arrowButton);
   }
 
   for (let i = 0; i < rows; i++) {
-    let rowNum = "row-" + i;
+    const rowNum = `row-${i}`;
 
-    let newRow = $("<div></div>").attr("class", "grid-row").attr("id", rowNum);
+    const newRow = $('<div></div>').attr('class', 'grid-row').attr('id', rowNum);
 
-    $("#grid").append(newRow);
-
+    $('#grid').append(newRow);
 
     for (let j = 0; j < columns; j++) {
-      //$(selector).attr({attribute:value, attribute:value,...})
+      // $(selector).attr({attribute:value, attribute:value,...})
 
-      let newColumn = $("<div></div>")
-        .attr("class", "grid-column")
-        .attr("id", "row-" + i + "-column-" + j)
+      const newColumn = $('<div></div>')
+        .attr('id', `row-${i}-column-${j}`)
         .append($("<span class='whitedot'></span>"));
 
-      $("#row-" + i).append(newColumn);
-
+      $(`#row-${i}`).append(newColumn);
     }
   }
 }
 
-
-// function resetClick(){
-//   numRows=6
-//   numColumns=7
-//   drawGrid()
+// function resetClick() {
+//   numRows = 6;
+//   numColumns = 7;
 // }
 
+getFormSize();
 
-//add event listener to reset button
-// document.getElementById("reset-btn").addEventListener('click', resetClick)
+// eslint-disable-next-line no-use-before-define
 
+// eslint-disable-next-line no-undef
+const arrowButtons = $('.arrow-grid');
+
+const arrowArray = Array.from(arrowButtons);
+
+for (let i = 0; i < arrowArray.length; i++) {
+  arrowArray[i].addEventListener('click', () => {
+    const arrowButton = i;
+
+    // eslint-disable-next-line no-restricted-globals
+    arrowClick(arrowButton, event);
+  });
+}
+
+// add event listener to reset button
+// $('#reset-btn').click(resetClick);
